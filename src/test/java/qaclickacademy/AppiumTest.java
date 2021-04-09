1694626349838b1a1ca34a6b6cf259df3f41dde9
yaswanthkumar <yaswanthkumarmunagapati@gmail.com>
2021-04-09 17:09:05 +0530

    Maven Project

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	
+	
+	@Test
+	public void NativeAppAndriod()
+	{
+		System.out.println("NativeAppAndriod");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	//TestNG xml file from Maven
+	//How to execute all test casws from test folder - mvn test
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("elementsUI");
+	}
+
+}
